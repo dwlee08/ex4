1ba5d0297f13f97966f031f7ee685c60c6081d35
Dongwoo Lee <dwlee08@gmail.com>
2022-11-22 18:23:52 -0800

    Test Main
    Signed-off-by: Dongwoo Lee <dwlee08@gmail.com>

--- Main.java
+++ Main.java
@@ -3,6 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Customer me = new Customer("DO");
+
+		Movie godfather = new Movie("God Father", Movie.CLASSIC);
+		Movie avatar = new Movie("Avatar", Movie.REGULAR);
+		Movie avengers = new Movie("Avengers", Movie.RELEASE);
+		
+		me.addRental(new Rental(14, godfather));
+		me.addRental(new Rental(3, avatar));
+		me.addRental(new Rental(7, avengers));
+
 		System.out.println(me.statement());
 	}
 }
